174f7ee00d53552bc88bcd6d079a8fb111efb0e6
Tamim Karim <ittamim22@gmail.com>
2023-12-16 17:01:29 +0600

    final commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            const { displayName, email } = result.user;
+            const signedInUser = { name: displayName, email }
+            setLoggedInUser(signedInUser);
+            history.replace(from);
+            console.log(signedInUser);
+
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        });
+
+    }
+
+
+    return (
+        <div>
+            <h1>This is Login page</h1>
+            <button onClick={handleGoogleSignIn} >Google Sign in</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAFPebWSJdUxuhvD7M1gJ-YGjNVxCxxlyk",
+  authDomain: "burj-al-arab-1659c.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-1659c.firebaseio.com",
+  projectId: "burj-al-arab-1659c",
+  storageBucket: "burj-al-arab-1659c.appspot.com",
+  messagingSenderId: "641174621167",
+  appId: "1:641174621167:web:ffd3c4e8dd0def192c441a"
+};
+export default firebaseConfig;
